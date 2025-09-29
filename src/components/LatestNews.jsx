984968b6324ac36002479736cf4ad5ae226edae2
Taricol <mozumder9122023@gmail.com>
2025-09-29 20:30:39 +0600

    nav bar added

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,7 +5,7 @@ const LatestNews = () => {
     return (
         <div className='flex items-center gap-5 bg-base-200 p-3'>
             <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
-            <Marquee className='flex gap-5' pauseOnHover={true}>
+            <Marquee className='flex gap-5' pauseOnHover={true} speed={60}>
                 <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
                 <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
                 <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
