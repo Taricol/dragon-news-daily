8744c216367607f5aa952b90f85ff6e402d7341d
Taricol <mozumder9122023@gmail.com>
2026-02-06 13:05:33 +0600

    headline updated

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,10 +5,13 @@ const LatestNews = () => {
     return (
         <div className='flex items-center gap-5 bg-base-200 p-3'>
             <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
-            <Marquee className='flex gap-5' pauseOnHover={true} speed={60}>
-                <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
-                <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
-                <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quasi.</p>
+            <Marquee className='flex gap-১০' pauseOnHover={true} speed={60}>
+                <p className='font-bold'>।।।আসিফ খেলো ছেকা  ।</p>
+                <p className='font-bold'>।।।এবার টেনশন এ পড়ে গেল তাহমিদ বিন আজিজ ওরফে পাশা   ।</p>
+                <p className='font-bold'>
+            ।।। বিশ্বের সেরা বাটপার হওয়ার স্বপ্ন ইয়ুশা ফয়সালের ।।।
+        
+                </p>
                 </Marquee>
             
         </div>
